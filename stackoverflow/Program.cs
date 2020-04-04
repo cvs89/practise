3c8339c7d1bfcc40a262646fdf07c0758400014d
cvs rathore <chandraveersingh.89@live.com>
2020-04-04 16:25:53 +0530

    add a saprate file for the programe

--- stackoverflow/Program.cs
+++ stackoverflow/Program.cs
@@ -2,43 +2,6 @@
 
 namespace stackoverflow
 {
-    class Post
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public DateTime Created { get; set; }
-        private int UpVote;
-        private int DownVote;
-
-        public Post(string title, string description)
-        {
-            this.Title = title;
-            this.Description = description;
-            this.Created = DateTime.Now;
-            this.UpVote = 0;
-            this.DownVote = 0;
-        }
-        public Post(string title, string description, DateTime created, int upVote, int downVote) : this(title, description)
-        {
-            Created = created;
-            UpVote = upVote;
-            DownVote = downVote;
-        }
-        public void SetPostUpVote()
-        {
-            this.UpVote += 1;
-        }
-        public void SetPostDownVote()
-        {
-            this.DownVote += 1;
-        }
-        public int GetPostVotes()
-        {
-            return this.UpVote - this.DownVote;
-        }
-
-        
-    }
     class Program
     {
         static void Main(string[] args)
