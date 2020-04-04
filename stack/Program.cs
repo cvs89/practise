5c6b54561ca936f5e4190ac8201afe21c51d9711
cvs rathore <chandraveersingh.89@live.com>
2020-04-04 16:57:42 +0530

    first commit

---
+++ stack/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+    
+namespace stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            try
+            {
+                //Console.WriteLine(stack.Pop());
+                stack.Clear();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            
+        }
+    }
+}
---
+++ stack/Stack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace stack
+{
+    class Stack
+    {
+        private List<object> data = new List<object>();
+
+        public void Push(object obj)
+        {
+            data.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if(data.Count == 0)
+            {
+                throw new InvalidOperationException("You are pulling from empty list");
+            }
+            var element = data[data.Count - 1];
+            data.RemoveAt(data.Count - 1);
+            return element;
+        }
+
+        public void Clear()
+        {
+            if (data.Count == 0)
+            {
+                throw new InvalidOperationException("You are clearing an empty list");
+            }
+            data.Clear();
+        }
+
+    }
+}
